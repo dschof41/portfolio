e2b55086c99a977c9346e7f71812731b10e5957f
dschof41 <dschof41@gmail.com>
2020-12-22 10:15:05 -0500

    Initialize site
    init site from starter
    add personal details

---
+++ portfolio-site/src/components/landing/Skills/index.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { ThemeContext } from 'providers/ThemeProvider';
+import { Container, Button } from 'components/common';
+import dev from 'assets/illustrations/skills.svg';
+import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
+
+export const Skills = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <Wrapper id="about">
+      <SkillsWrapper as={Container}>
+        <Thumbnail>
+          <img src={dev} alt="I’m Dan and I’m a Backend & Devops engineer!" />
+        </Thumbnail>
+        <Details theme={theme}>
+          <h1>About me</h1>
+          <p>
+            I work on large scale web projects in the insurance industry.
+          </p>
+          <Button as={AnchorLink} href="#contact">
+            Hire me
+          </Button>
+        </Details>
+      </SkillsWrapper>
+    </Wrapper>
+  );
+};
